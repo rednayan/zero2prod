7b7c5e0cf12df160bd9c7c5d9bbf520652fda795
nayan <nayansharma1010@gmail.com>
2023-04-20 17:58:40 +0530

    form data deserialize

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -16,6 +16,46 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=name=nayan%20sharma&email=nayan1010%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("execute post request success.");
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_form_data() {
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=nayan", "missing email"),
+        ("missing name", "email=nayan%40gmail.com"),
+        ("", "missing both email and name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("execute post data success");
+
+        assert_eq!(400, response.status().as_u16(), "ERROR: {error_message}");
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("bind random port success");
     let port = listener.local_addr().unwrap().port();
