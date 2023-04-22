0fa1ccbfd21ce06e97dbffeb2b4baed4f3156e91
nayan <nayansharma1010@gmail.com>
2023-04-22 13:51:54 +0530

    chmod exe

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,13 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     .run();
     Ok(server)
 }
+// pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+//     let server = HttpServer::new(|| {
+//         App::new()
+//             .route("/health_check", web::get().to(health_check))
+//             .route("/subscriptions", web::post().to(subscribe))
+//     })
+//     .listen(listener)?
+//     .run();
+//     Ok(server)
+// }
