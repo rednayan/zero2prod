75bf48954bdab669eff595234bfcc18c3f2a6755
nayan <nayansharma1010@gmail.com>
2023-04-27 16:23:05 +0530

    spans

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use serde::Deserialize;
 use sqlx::PgPool;
+use tracing::Instrument;
 use uuid::Uuid;
 
 #[derive(Deserialize)]
@@ -12,16 +13,18 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    tracing::info!(
-        "request_id: {} : Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+
+    let request_span = tracing::info_span!(
+        "Adding a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    tracing::info!(
-        "request_id: {} : Saving new subscriber details in the database",
-        request_id
-    );
+
+    let _request_span_guard = request_span.enter();
+
+    let query_span = tracing::info_span!("Saving new subscriber details in the database");
+
     match sqlx::query!(
         r#"
             INSERT INTO subscriptions (id,email,name,subscribed_at) VALUES ($1,$2,$3,$4)    
@@ -32,21 +35,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         Utc::now()
     )
     .execute(pool.get_ref())
+    .instrument(query_span)
     .await
     {
-        Ok(_) => {
-            tracing::info!(
-                "request_id: {} : New subscriber details have been saved",
-                request_id
-            );
-            HttpResponse::Ok().finish()
-        }
+        Ok(_) => HttpResponse::Ok().finish(),
         Err(err) => {
-            tracing::error!(
-                "request_id : {} : Failed to execute query: {:?}",
-                request_id,
-                err
-            );
+            tracing::error!("Failed to execute query:{:?}", err);
             HttpResponse::InternalServerError().finish()
         }
     }
