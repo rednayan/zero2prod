31631e9df863a579c425e86eb74d8cc3ff0cf235
nayan <nayansharma1010@gmail.com>
2023-04-28 14:00:36 +0530

    docker image X offline sqlx

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
 use zero2prod::{configuration::get_configuration, startup, telemetry};
@@ -7,9 +8,10 @@ async fn main() -> std::io::Result<()> {
     let subscriber = telemetry::get_subscriber("zero2prod".into(), "info".into(), std::io::stdout);
     telemetry::init_subscriber(subscriber);
     let configuration = get_configuration().expect("Failed to read  configuration");
-    let connection_pool = PgPool::connect(&configuration.database.connection_string())
-        .await
-        .expect("Failed to connect to Postgres");
+    let connection_pool =
+        PgPool::connect(&configuration.database.connection_string().expose_secret())
+            .await
+            .expect("Failed to connect to postgres");
     let address = format!("127.0.0.1:{port}", port = configuration.application_port);
     let listener = TcpListener::bind(address)?;
     startup::run(listener, connection_pool)?.await
